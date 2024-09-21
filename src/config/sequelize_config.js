ee761857c431350788801b3fa05c2bf0cdeedd54
akshay22bv <akshay22bv@gmail.com>
2024-09-21 13:52:42 +0530

    sequlize added

--- src/config/sequelize_config.js
+++ src/config/sequelize_config.js
@@ -1,15 +1,15 @@
-// const { Sequelize } = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-// // Initialize Sequelize using environment variables
+// Initialize Sequelize using environment variables
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true, // Ensure SSL connection on production
-//       rejectUnauthorized: false, // If Vercel requires SSL, this bypasses certificate validation
-//     },
-//   },
-// });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true, // Ensure SSL connection on production
+      rejectUnauthorized: false, // If Vercel requires SSL, this bypasses certificate validation
+    },
+  },
+});
 
-// module.exports = sequelize;
+module.exports = sequelize;
